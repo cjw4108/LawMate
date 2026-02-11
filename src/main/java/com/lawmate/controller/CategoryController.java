fb9dac308ab26a6c1ec31cf5fabec75702c57e18
한기현 <han960409@naver.com>
2026-02-11 10:05:34 +0900

    123

---
+++ src/main/java/com/lawmate/controller/CategoryController.java
@@ -0,0 +1,13 @@
+package com.lawmate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CategoryController {
+
+    @GetMapping("/category")
+    public String category() {
+        return "category"; // → /WEB-INF/views/ccategory.jsp
+    }
+}
