0b62c848b79b3ee18defb324fe75b24c739e8090
한기현 <han960409@naver.com>
2026-02-11 17:06:47 +0900

    바보같이 @Controller 기입 안했음

--- src/main/java/com/lawmate/controller/SearchController.java
+++ src/main/java/com/lawmate/controller/SearchController.java
@@ -1,7 +1,9 @@
 package com.lawmate.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class SearchController {
 
     @GetMapping("/search")
